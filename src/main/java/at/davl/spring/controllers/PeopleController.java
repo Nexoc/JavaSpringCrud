c9861c9098ea7f6ead9d6f6e4f08f621ae7452f1
Marat <davletshinm@list.ru>
2024-01-11 13:01:16 +0100

    Rest initialisation

---
+++ src/main/java/at/davl/spring/controllers/PeopleController.java
@@ -0,0 +1,35 @@
+package at.davl.spring.controllers;
+
+import at.davl.spring.dao.PersonDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model) {
+        // get people from DAO and send forward
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id, Model model) {
+        // get 1 person by ID from DAO and send forward to the frontend
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
