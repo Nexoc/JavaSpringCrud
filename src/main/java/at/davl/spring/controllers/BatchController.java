8afa3a7751b04c5594b8e02067fde18c8382a71e
Marat <davletshinm@list.ru>
2024-01-15 15:11:55 +0100

    BatchController

---
+++ src/main/java/at/davl/spring/controllers/BatchController.java
@@ -0,0 +1,36 @@
+package at.davl.spring.controllers;
+
+import at.davl.spring.dao.PersonDAO;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/test-batch-update")
+public class BatchController {
+
+    public final PersonDAO personDAO;
+
+    public BatchController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index() {
+        return "batch/index";
+    }
+
+    @GetMapping("/without")
+    public String withoutBatch() {
+        personDAO.testMultipleUpdate();
+        return "redirect:/people";
+    }
+
+    @GetMapping("/Crud_war/with")
+    public String withBatch() {
+        personDAO.testBatchUpdate();
+        return "redirect:/people";
+    }
+
+
+}
